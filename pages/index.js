a138fd0b77a41920cf2e7b548649451bc2740714
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-09-06 12:46:17 +0300

    Changes

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        EXAMPLE APP
+          EXAMPLE APP
         </h1>
 
         <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </p>     
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </main>
 
       <footer className={styles.footer}>
@@ -27,8 +27,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by {'Maureen'}
-          
+          End
         </a>
       </footer>
     </div>
